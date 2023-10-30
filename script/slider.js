ff04ba9c95582e568c10e6a9b9b0c6ea3140a1d7
Alessio Scarfone <alessioscarfone93@gmail.com>
2023-10-30 18:17:24 +0100

    feat: update slider + shop now btn

--- script/slider.js
+++ script/slider.js
@@ -1,10 +1,9 @@
 const prevBtn = document.querySelector('.slider-btn.prev');
 const nextBtn = document.querySelector('.slider-btn.next');
 
-
-
 const changeImage = (direction) => () => {
     const pictures = document.querySelectorAll('.hero-imgs picture');
+    const texts = document.querySelectorAll('.slider-text-container .text');
     
     let visibleIndex = 0;
     pictures.forEach((pic, i) => {
@@ -14,9 +13,11 @@ const changeImage = (direction) => () => {
     });
 
     pictures[visibleIndex].classList.add('d-hide');
+    texts[visibleIndex].classList.add('d-hide');
     let toShow = direction == 'next' ? mod((visibleIndex + 1), (pictures.length))
         : mod((visibleIndex - 1), (pictures.length));
     pictures[toShow].classList.remove('d-hide');  
+    texts[toShow].classList.remove('d-hide');  
 }
 
 const mod = (x,m) => {
